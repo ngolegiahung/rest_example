33d18db7bf0d8d243b67ff5f29c33bdb4e52e8fb
Hung L.G. NGO <hung.l.g.ngo@gmail.com>
2024-10-01 20:32:16 +0700

    Init project.

---
+++ src/main/java/com/restclient/rest_client_examples/RestClientExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.restclient.rest_client_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restclient/rest_client_examples/RestClientExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restclient.rest_client_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
